bb096d1159be61f4a38b3e9cc001464fbe17da85
Brian Chang <brian.chang@binus.ac.id>
2026-06-16 22:03:36 +0700

    Initial Commit

---
+++ iPoop/iPoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPoopApp.swift
+//  iPoop
+//
+//  Created by Brian Chang on 16/06/26.
+//
+
+import SwiftUI
+
+@main
+struct iPoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
