d048c3bdae91e401fbfe31d4dd74721be4cc5e60
Brian Chang <brian.chang@binus.ac.id>
2026-06-21 00:14:13 +0700

    Add: Edit Poop Page

---
+++ iPoop/View/EditPoop.swift
@@ -0,0 +1,144 @@
+//
+//  EditPoop.swift
+//  iPoop
+//
+//  Created by Brian Chang on 20/06/26.
+//
+
+import SwiftUI
+
+struct EditPoop: View {
+    var body: some View {
+        NavigationStack{
+            VStack (alignment: .leading) {
+                PoopCard()
+                
+                Text("Edit Poop Detail")
+                    .font(.body)
+                    .padding(.top, 16)
+                    .padding(.bottom, 8)
+                
+                VStack (spacing: 8) {
+                    VStack (alignment: .leading, spacing: 4) {
+                        HStack {
+                            Text("Poop Name")
+                            Spacer()
+                            Image(systemName: "square.and.pencil")
+                        }
+                        .font(.subheadline)
+                        Text("GoPoop")
+                            .font(.footnote)
+                    }
+                    .padding(16)
+                    .background(
+                        RoundedRectangle(cornerRadius: 8, style: .continuous)
+                            .fill(Color(.systemBackground))
+                    )
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8, style: .continuous)
+                            .stroke(Color.black, lineWidth: 2)
+                    )
+                    
+                    VStack (alignment: .leading, spacing: 4) {
+                        HStack {
+                            Text("Poop Description")
+                            Spacer()
+                            Image(systemName: "square.and.pencil")
+                        }
+                        .font(.subheadline)
+                        Text("GoPoop")
+                            .font(.footnote)
+                    }
+                    .padding(16)
+                    .background(
+                        RoundedRectangle(cornerRadius: 8, style: .continuous)
+                            .fill(Color(.systemBackground))
+                    )
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8, style: .continuous)
+                            .stroke(Color.black, lineWidth: 2)
+                    )
+                    VStack (alignment: .leading, spacing: 4) {
+                        HStack {
+                            Text("Food")
+                            Spacer()
+                            Image(systemName: "square.and.pencil")
+                        }
+                        .font(.subheadline)
+                        Text("GoPoop")
+                            .font(.footnote)
+                    }
+                    .padding(16)
+                    .background(
+                        RoundedRectangle(cornerRadius: 8, style: .continuous)
+                            .fill(Color(.systemBackground))
+                    )
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8, style: .continuous)
+                            .stroke(Color.black, lineWidth: 2)
+                    )
+                    VStack (alignment: .leading, spacing: 4) {
+                        HStack {
+                            Text("Drink")
+                            Spacer()
+                            Image(systemName: "square.and.pencil")
+                        }
+                        .font(.subheadline)
+                        Text("GoPoop")
+                            .font(.footnote)
+                    }
+                    .padding(16)
+                    .background(
+                        RoundedRectangle(cornerRadius: 8, style: .continuous)
+                            .fill(Color(.systemBackground))
+                    )
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8, style: .continuous)
+                            .stroke(Color.black, lineWidth: 2)
+                    )
+                }
+                
+                Button(action: {
+                }) {
+                    Text("Save Poop")
+                        .font(.system(size: 16, weight: .medium))
+                        .foregroundColor(.white)
+                        .padding(.vertical, 12)
+                        .padding(.horizontal, 40)
+                        .background(
+                            Capsule()
+                                .tint(.blue)
+                        )
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+                .padding(.vertical, 24)
+                
+                Spacer()
+            }
+            .frame(alignment: .top)
+            .padding(16)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    HStack(spacing: 15) {
+                        Image(systemName: "toilet")
+                    }
+                    .font(.title3)
+                    .foregroundColor(.black)
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    HStack(spacing: 15) {
+                        Image(systemName: "person.circle")
+                        Image(systemName: "bell")
+                    }
+                    .font(.title3)
+                    .foregroundColor(.black)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    EditPoop()
+}
--- iPoop/View/LogPageView.swift
+++ iPoop/View/LogPageView.swift
@@ -7,70 +7,74 @@
 import SwiftUI
 
 struct LogPageView: View {
+    @State private var goToEdit = false
     var body: some View {
-        TabView {
-            
-            NavigationStack {
-                VStack {
-                    HStack {
-                        VStack(alignment: .leading, spacing: 2) {
-                            Text("Log Your Poop!")
-                                .font(.system(size: 36, weight: .bold))
-                                .foregroundColor(.black)
-                            
-                            Text("Click Add to create log of your poop!")
-                                .font(.caption)
-                                .foregroundColor(.gray)
-                        }
-                        Spacer()
-                    }
-                    .padding(.horizontal)
-                    .padding(.top, 16)
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 16)
-                            .fill(Color(.systemGray6))
+        
+        NavigationStack {
+            VStack {
+                HStack {
+                    VStack(alignment: .leading, spacing: 2) {
+                        Text("Log Your Poop!")
+                            .font(.system(size: 36, weight: .bold))
+                            .foregroundColor(.black)
                         
-                        Text("Gambar Poop")
+                        Text("Click Add to create log of your poop!")
                             .font(.caption)
                             .foregroundColor(.gray)
                     }
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 400)
-                    .padding(.horizontal)
-                    
-                    Button(action: {
-                    }) {
-                        Text("Add Poop")
-                            .font(.system(size: 16, weight: .medium))
-                            .foregroundColor(.white)
-                            .padding(.vertical, 12)
-                            .padding(.horizontal, 40)
-                            .background(
-                                Capsule()
-                                    .tint(.blue)
-                            )
-                    }
-                    .padding(50)
-                    
                     Spacer()
                 }
-                .toolbar {
-                    ToolbarItem(placement: .topBarLeading) {
-                        HStack(spacing: 15) {
-                            Image(systemName: "toilet")
-                        }
-                        .font(.title3)
-                        .foregroundColor(.black)
-                    }
+                .padding(.horizontal)
+                .padding(.top, 16)
+                ZStack {
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill(Color(.systemGray6))
                     
-                    ToolbarItem(placement: .topBarTrailing) {
-                        HStack(spacing: 15) {
-                            Image(systemName: "person.circle")
-                            Image(systemName: "bell")
-                        }
-                        .font(.title3)
-                        .foregroundColor(.black)
+                    Text("Gambar Poop")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+                .frame(maxWidth: .infinity)
+                .frame(height: 400)
+                .padding(.horizontal)
+                
+                Button(action: {
+                    goToEdit = true
+                }) {
+                    Text("Add Poop")
+                        .font(.system(size: 16, weight: .medium))
+                        .foregroundColor(.white)
+                        .padding(.vertical, 12)
+                        .padding(.horizontal, 40)
+                        .background(
+                            Capsule()
+                                .tint(.blue)
+                        )
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+                .padding(.vertical, 24)
+                
+                Spacer()
+            }
+            .navigationDestination(isPresented: $goToEdit) {
+                EditPoop()
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    HStack(spacing: 15) {
+                        Image(systemName: "toilet")
                     }
+                    .font(.title3)
+                    .foregroundColor(.black)
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    HStack(spacing: 15) {
+                        Image(systemName: "person.circle")
+                        Image(systemName: "bell")
+                    }
+                    .font(.title3)
+                    .foregroundColor(.black)
                 }
             }
         }
